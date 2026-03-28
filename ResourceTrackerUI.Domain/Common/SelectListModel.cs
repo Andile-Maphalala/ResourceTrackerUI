3fb5e07862af98eede5b3c662484474aa6f41398
Andile-Maphalala <andile.maphalala.am@gmail.com>
2026-03-28 15:13:31 +0200

    Created resdubale custom componet for dropdowns. Updated custom table to swap buttons

---
+++ ResourceTrackerUI.Domain/Common/SelectListModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace ResourceTrackerUI.Domain.Common
+{
+    public class SelectListModel<T>
+    {
+        public T Value { get; set; }
+        public string Text { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
